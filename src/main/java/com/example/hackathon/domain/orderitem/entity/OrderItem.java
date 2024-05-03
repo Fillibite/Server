d2e8ab6fd2fcc9b6ac11a271b2d3e8b95997d24f
JJiyunkim <jy901504@naver.com>
2024-05-04 03:22:36 +0900

    [FEAT] : OrderItem entity 추가

---
+++ src/main/java/com/example/hackathon/domain/orderitem/entity/OrderItem.java
@@ -0,0 +1,36 @@
+package com.example.hackathon.domain.orderitem.entity;
+
+import com.example.hackathon.domain.order.entity.Order;
+import com.example.hackathon.domain.item.entity.Item;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Getter
+@Setter
+@Builder // 빌더 패턴 적용
+@NoArgsConstructor // 기본 생성자 생성
+@AllArgsConstructor // 모든 필드를 가진 생성자 생성
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment (기본키)
+    private Long id;
+
+    @Column
+    private String orderprice;
+
+    @Column
+    private Long count;
+
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "itemId", referencedColumnName = "id")
+    private Item item;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "orderId", referencedColumnName = "id")
+    private Order order;
+
+}
